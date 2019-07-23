25952b5dea17fe174cfbc5caf090cc378741d7fa
ivan.heibi <ivanhb.ita@gmail.com>
2019-07-23 16:21:56 +0200

    dharc js

--- asset/js/dharc_main.js
+++ asset/js/dharc_main.js
@@ -1,6 +1,9 @@
+(function($) {
+    var PAGE_ITEM = "https://137.204.168.8/s/digital-library/item";
 
-var PAGE_ITEM = "https://137.204.168.8/s/digital-library/item";
+    $(document).ready(function() {
+        /*Dynamic Blocks to add*/
+        $("<a href='"+PAGE_ITEM+"'>Test</p>" ).insertBefore( "div .item-showcase" );
+    });
 
-
-/*Dynamic Blocks to add*/
-$("<a href='"+PAGE_ITEM+"'>Test</p>" ).insertBefore( "div .item-showcase" );
+})(jQuery);
