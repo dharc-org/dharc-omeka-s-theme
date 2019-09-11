8091c92a822b85837dc9d276d9ee784c248975b8
ivan.heibi <ivanhb.ita@gmail.com>
2019-09-11 17:51:57 +0200

    view link

--- asset/js/dharc_main.js
+++ asset/js/dharc_main.js
@@ -4,4 +4,7 @@ var PAGE_ITEM = "../item";
 $(document).ready(function() {
         /*Dynamic Blocks to add*/
         $("<div class='item-showcase-title'><a href='"+PAGE_ITEM+"'>The Digital Library items &#10509;</a><div>" ).insertBefore( "div .item-showcase" );
+
+        $("<a href='"+PAGE_ITEM+"/view'>").insertBefore( "img .item-img");
+        $("</a>").insertAfter( "img .item-img");
 });
