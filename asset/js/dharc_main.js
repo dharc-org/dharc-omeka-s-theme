36f82ad72cc282bb1b1b5c359e5cf519418a0976
tech <tech@DHARC-OMEKA.personale.dir.unibo.it>
2019-09-12 12:31:56 +0200

    new view page

--- asset/js/dharc_main.js
+++ asset/js/dharc_main.js
@@ -11,3 +11,15 @@ $(document).ready(function() {
         $(".item-img").wrap("<a href='"+String(window.location.href)+"/view.html'></a>");
 
 });
+
+function init_view(){
+  $("body").html("");
+  $("body").addClass("view-mirador");
+  //mirador css
+  $("body").append('<link href="/modules/Mirador/asset/vendor/mirador/css/mirador-combined.min.css?v=3.1.1" media="screen" rel="stylesheet" type="text/css">');
+  $("body").append('<link href="/modules/Mirador/asset/css/mirador.css?v=3.1.1" media="screen" rel="stylesheet" type="text/css">');
+  //mirador js
+  $("body").append('<script type="text/javascript" src="/modules/Mirador/asset/vendor/mirador/mirador.min.js?v=3.1.1"></script>');
+  $("body").append('<script type="text/javascript">$(function() {Mirador({ "id": "mirador-view",      "buildPath": "/modules/Mirador/asset/vendor/mirador/",      "locale": "en_US",      "data": [          {              "manifestUri": "http://137.204.168.8/iiif/545/manifest"          }      ],      "windowObjects": [          {              "loadedManifest": "http://137.204.168.8/iiif/545/manifest"          }      ]  });  });  </script>');
+  $("body").append('<div id="mirador-view" class="mirador viewer"></div>');
+}
