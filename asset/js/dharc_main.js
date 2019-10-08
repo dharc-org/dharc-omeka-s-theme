7e191e651bfef5a935dfcda9aa4c6964ae6881be
tech <tech@DHARC-OMEKA.personale.dir.unibo.it>
2019-10-08 10:36:26 +0200

    add js for easy migration

--- asset/js/dharc_main.js
+++ asset/js/dharc_main.js
@@ -3,6 +3,7 @@ var PAGE_ITEM = "../item";
 
 var BASE_PAGE = "http://137.204.168.8/s/digital-library/page/";
 var BASE_URL = "http://137.204.168.8/iiif/";
+var BASE_PATH = "/";
 
 $(document).ready(function() {
         $("<div class='item-showcase-title'><a href='"+PAGE_ITEM+"'>The Digital Library items &#10509;</a><div>" ).insertBefore( "div .item-showcase" );
@@ -17,8 +18,8 @@ function init_mirador_item_link() {
 
   $(".item-view-button").attr("href", BASE_PAGE+"view"+"?id="+item_id+"&type=item");
   
-  $('.item-iiif').prepend('<img class="item-iiif-img" />')
-  $(".item-iiif-img").wrap("<a href='https://137.204.168.8/iiif/"+item_id+"/manifest'></a>");
+  $('.item-iiif').prepend('<img class="item-iiif-img" />');
+  $(".item-iiif-img").wrap("<a href='"+BASE_URL+item_id+"/manifest'></a>");
 
 }
 
@@ -58,7 +59,7 @@ function init_mirador_config() {
   }
   var mirador_config = {
         "id": "mirador-view",
-        "buildPath": "/modules/Mirador/asset/vendor/mirador/",
+        "buildPath": BASE_PATH+"modules/Mirador/asset/vendor/mirador/",
         "locale": "en_US"
   };
   return Object.assign(mirador_config, mirador_item_obj);
@@ -69,10 +70,10 @@ function init_view(){
   $("body").html("");
   $("body").addClass("view-mirador");
   //mirador css
-  $("body").append('<link href="/modules/Mirador/asset/vendor/mirador/css/mirador-combined.min.css?v=3.1.1" media="screen" rel="stylesheet" type="text/css">');
-  $("body").append('<link href="/modules/Mirador/asset/css/mirador.css?v=3.1.1" media="screen" rel="stylesheet" type="text/css">');
+  $("body").append('<link href="'+BASE_PATH+'modules/Mirador/asset/vendor/mirador/css/mirador-combined.min.css?v=3.1.1" media="screen" rel="stylesheet" type="text/css">');
+  $("body").append('<link href="'+BASE_PATH+'modules/Mirador/asset/css/mirador.css?v=3.1.1" media="screen" rel="stylesheet" type="text/css">');
   //mirador js
-  $("body").append('<script type="text/javascript" src="/modules/Mirador/asset/vendor/mirador/mirador.min.js?v=3.1.1"></script>');
+  $("body").append('<script type="text/javascript" src="'+BASE_PATH+'modules/Mirador/asset/vendor/mirador/mirador.min.js?v=3.1.1"></script>');
   $("body").append('<script type="text/javascript">$(function() {Mirador('+JSON.stringify(MIRADOR_CONFIG)+');  });  </script>');
   $("body").append('<div id="mirador-view" class="mirador viewer"></div>');
 }
